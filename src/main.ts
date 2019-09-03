d8613ff51b25d8107075e4f5eceb599929dc981f
mkdorff <mkdorff@outlook.com>
2019-09-03 08:11:00 -0600

    output

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import * as core from '@actions/core';
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  core.setOutput('version', '2.2.2')
 }
 
 run();
