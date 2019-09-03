b0c86534f8b005cf37b492ea2eab04f7ba3ac32c
mkdorff <mkdorff@outlook.com>
2019-09-03 08:28:27 -0600

    done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import * as core from "@actions/core";
 
 async function run() {
-  console.log(require(process.cwd() + "/package.json"));
-  core.setOutput("version", require(process.cwd() + "/package.json").versions);
+  core.setOutput("version", require(process.cwd() + "/package.json").version);
 }
 
 run();
